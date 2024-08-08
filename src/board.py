3913d5610f2a7ea3a45dc2a8c69688b8c1995e60
Joshua Park <123624726+joshprk@users.noreply.github.com>
2024-08-08 00:11:18 -0400

    fen parsing complete--ill test this later lol

--- src/board.py
+++ src/board.py
@@ -1,3 +1,4 @@
+from calendar import c
 from dataclasses import dataclass
 from enum import Enum, Flag
 from typing import Iterator
@@ -28,6 +29,7 @@ class Side(Flag):
     WHITE = (0,)
     BLACK = (1,)
 
+
 @dataclass
 class CastlingRights:
     white_king: bool
@@ -36,18 +38,35 @@ class CastlingRights:
     black_queen: bool
 
 
+@dataclass
+class FromMoveStr:
+    from_idx: int
+    to_idx: int
+    promotion: Piece
+
+
 class Board:
     bitboards: np.array[np.uint64]
     side_to_move: Side
-
+    castling: CastlingRights
+    ep_state: int
+    halfmove: int
+    fullmove: int
+    
     def __init__(self, fen: str | Iterator[Match[str]] = START_FEN):
         if type(fen) is str:
             fen = re.finditer(r"\S+", fen)
         elif type(fen) is not Iterator[Match[str]]:
             raise Exception("Invalid FEN type")
 
-        self.bitboards = self.parse_board(next(fen))
-        self.side_to_move = Side.WHITE if next(fen) == "w" else Side.BLACK
+        self.bitboards = self.parse_board(next(fen).string)
+        self.side_to_move = Side.WHITE if next(fen).string == "w" else Side.BLACK
+        self.castling = self.parse_castling(next(fen).string)
+        
+        ep_str = next(fen).string
+        self.ep_state = -1 if ep_str == "-" else (ord(ep_str[0].lower()) - ord('a')) + int(ep_str[1]) * 8
+        self.halfmove = int(next(fen).string)
+        self.fullmove = int(next(fen).string)
 
     def parse_board(board: str) -> np.array[np.uint64]:
         bitboards = np.array([0 for _ in range(64)], dtype=np.uint64)
@@ -112,5 +131,40 @@ class Board:
                     raise Exception("Malformed castling rights")
 
         return rights
-    
-    
+
+
+def parse_move(move: str) -> FromMoveStr:
+    if move == "0000":
+        return FromMoveStr(-1, -1, Piece.NULL_PIECE)
+
+    from_x = ord(move[0].lower()) - ord('a')
+    from_y = int(move[1])
+    to_x = ord(move[2].lower()) - ord('a')
+    to_y = int(move[3])
+
+    from_idx = from_x + from_y * 8
+    to_idx = to_x + to_y * 8
+    promotion = Piece.NULL_PIECE
+
+    if len(move) > 4:
+        match move[4]:
+            case "Q":
+                promotion = Piece.WHITE_QUEEN
+            case "R":
+                promotion = Piece.WHITE_ROOK
+            case "N":
+                promotion = Piece.WHITE_KNIGHT
+            case "B":
+                promotion = Piece.WHITE_BISHOP
+            case "q":
+                promotion = Piece.BLACK_QUEEN
+            case "r":
+                promotion = Piece.BLACK_ROOK
+            case "n":
+                promotion = Piece.BLACK_KNIGHT
+            case "b":
+                promotion = Piece.BLACK_BISHOP
+            case _:
+                raise Exception("malformed movestr")
+
+    return FromMoveStr(from_idx, to_idx, promotion)
